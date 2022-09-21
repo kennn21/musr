4c71abd206eab4792cfe6f6a68b6e07a4476e74a
kennn21 <91940623+kennn21@users.noreply.github.com>
2022-09-22 00:29:38 +0700

    Added and implemented Delayed SplashScreen with: - SplashActivity.kt - activity_splash.xml - AndroidManifest.xml

--- app/src/main/java/com/example/musr/MainActivity.kt
+++ app/src/main/java/com/example/musr/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.musr
-
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
 
